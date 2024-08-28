32df19f2c09c7b77f600c5a9e618aec9f9aebdd2
milmilkim <65494214+milmilkim@users.noreply.github.com>
2024-08-28 17:02:12 +0900

    feat: 상태 토글

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import Pototo from './Pototo';
 
 const App = () => {
+  const [status, setStatus] = useState('home');
+
   return (
     <>
-      <Pototo />
+      <button
+        onClick={() =>
+          setStatus((prev) => (prev === 'home' ? 'canvas' : 'home'))
+        }>
+        toggle
+      </button>
+      {status === 'home' ? (
+        <div>
+          <h1>home</h1>
+        </div>
+      ) : (
+        <Pototo />
+      )}
     </>
   );
 };
